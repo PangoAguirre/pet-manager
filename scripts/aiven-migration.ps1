4450e4e7cec4b11c892e2992f9549e32d54780dc
Camilo <137337272+caamilo03@users.noreply.github.com>
2025-06-08 11:40:05 -0500

    fix: remove secrets from config files for GitHub security

--- scripts/aiven-migration.ps1
+++ scripts/aiven-migration.ps1
@@ -9,7 +9,7 @@ $AIVEN_HOST = "pet-manager-filmverse-db.c.aivencloud.com"
 $AIVEN_PORT = "10898"
 $AIVEN_DB = "defaultdb"
 $AIVEN_USER = "avnadmin"
-$AIVEN_PASSWORD = "AVNS_SlYBLMRNIYyXuTt1oju"
+$AIVEN_PASSWORD = "PLACEHOLDER_PASSWORD"  # CAMBIAR POR PASSWORD REAL
 $AIVEN_SSL = "require"
 
 Write-Host "Configuracion de conexion:" -ForegroundColor Blue
