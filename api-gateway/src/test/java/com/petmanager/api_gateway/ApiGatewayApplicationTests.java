5c82ca5fa946a295e9420e9914ddc0f729c96e0a
jose643415 <108759315+jose643415@users.noreply.github.com>
2025-06-16 16:44:13 -0500

    Se agregran test para api-gateway

--- api-gateway/src/test/java/com/petmanager/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/petmanager/api_gateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,31 @@
 package com.petmanager.api_gateway;
 
+import com.petmanager.api_gateway.config.CorsConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class ApiGatewayApplicationTests {
+class CorsConfigTest {
 
 	@Test
-	void contextLoads() {
+	void corsWebFilter_shouldBeCreatedSuccessfully() {
+		CorsConfig config = new CorsConfig();
+		CorsWebFilter filter = config.corsWebFilter();
+		assertNotNull(filter, "El CorsWebFilter no debería ser null");
 	}
+}
 
+@SpringBootTest
+class CorsConfigIntegrationTest {
+
+	@Autowired
+	private CorsWebFilter corsWebFilter;
+
+	@Test
+	void contextLoadsAndCorsWebFilterIsPresent() {
+		assertNotNull(corsWebFilter, "CorsWebFilter debería estar registrado en el contexto");
+	}
 }
