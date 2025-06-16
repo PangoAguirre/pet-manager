0d91ed5ff50033ca788cfd901026e9f457404737
jose643415 <108759315+jose643415@users.noreply.github.com>
2025-06-16 13:51:01 -0500

    Se configuran entornos de pruebas

--- notification-service/src/test/java/com/petmanager/notification_service/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/petmanager/notification_service/NotificationServiceApplicationTests.java
@@ -1,9 +1,10 @@
 package com.petmanager.notification_service;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NotificationServiceApplicationTests {
 
 	@Test
